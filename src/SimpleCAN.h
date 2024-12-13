b2a762e15cebcfc0da80a9dbb228eb58c28ff1d6
Rafik B <rafik24@yahoo.com>
2024-12-13 11:38:40 +0000

    Fixed the issue with HAL_FDCAN redefinition

--- src/SimpleCAN.h
+++ src/SimpleCAN.h
@@ -1,11 +1,18 @@
 #if defined(ARDUINO_ARCH_ESP32)
 #include "esp/twai/CAN.h"
-#elif defined(HAL_CAN_MODULE_ENABLED)
-#include "stm/can/CAN.h"
-#elif defined(HAL_FDCAN_MODULE_ENABLED)
-#include "stm/fdcan/CAN.h"
+#elif defined(ARDUINO_ARCH_STM32)
+    #if defined(STM32G4xx) || defined(STM32H7xx) || defined(STM32L5xx)
+        #ifndef HAL_FDCAN_MODULE_ENABLED
+            #define HAL_FDCAN_MODULE_ENABLED
+        #endif
+        #include "stm/fdcan/CAN.h"
+    #elif defined(HAL_CAN_MODULE_ENABLED)
+        #include "stm/can/CAN.h"
+    #else
+        #error "This STM32 family does not support CAN or FDCAN"
+    #endif
 #elif defined(ARDUINO_ARCH_GD32)
 #include "gd/can2b/CAN.h"
 #else
-#error "No CAN module is enabled, expecting a define for ARDUINO_ARCH_ESP32 | HAL_CAN_MODULE_ENABLED | HAL_FDCAN_MODULE_ENABLED"
+#error "No CAN module is enabled, expecting a define for ARDUINO_ARCH_ESP32 | ARDUINO_ARCH_STM32 | ARDUINO_ARCH_GD32"
 #endif
